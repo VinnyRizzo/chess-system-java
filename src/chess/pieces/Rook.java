ef677e4badc666cd2e9e7602544fa98a4af2bf9c
Vinny Rizzo <viniciusbragarizzo@gmail.com>
2025-07-23 09:49:49 -0300

    ChessException and ChessPosition

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -4,11 +4,10 @@ import boardgame.Board;
 import chess.ChessPiece;
 import chess.Color;
 
-public class Rook extends ChessPiece{
+public class Rook extends ChessPiece {
 	
 	public Rook(Board board, Color color) {
 		super(board, color);
-		
 	}
 	
 	@Override
